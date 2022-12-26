bf902ae95ab93d6c73da38c5a93120b15d60f6a1
Edye Quisca <edye.1230@gmail.com>
2022-12-26 13:43:12 -0500

    Spring Boot v1

---
+++ src/main/java/com/miprimerspring1/MiPrimerSpring1Application.java
@@ -0,0 +1,16 @@
+package com.miprimerspring1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiPrimerSpring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiPrimerSpring1Application.class, args);
+
+		
+		
+	}
+
+}
